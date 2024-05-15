3ec44c2c6add7706f7a22e943da7120e84c21460
aiaiaiai1 <cvc100326@naver.com>
2024-05-15 21:23:14 +0900

    feat: add skeleton of workspace controller

---
+++ src/main/java/gymmi/controller/WorkspaceController.java
@@ -0,0 +1,62 @@
+package gymmi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WorkspaceController {
+
+    @PostMapping("/workspaces")
+    public ResponseEntity<Void> createWorkspace() {
+        return null;
+    }
+
+    @GetMapping("/workspaces")
+    public ResponseEntity<Void> seeJoinedWorkspaces() {
+        return null;
+    }
+
+    @GetMapping("/workspaces")
+    public ResponseEntity<Void> seeAllWorkspaces() {
+        return null;
+    }
+
+    @PostMapping("/workspaces/{workspaceId}/join")
+    public ResponseEntity<Void> joinWorkspaces() {
+        return null;
+    }
+
+    @PostMapping("/workspaces/{workspaceId}/join/task")
+    public ResponseEntity<Void> writeTaskInWorkspace() {
+        return null;
+    }
+
+    @PatchMapping("/workspaces/{workspaceId}/start")
+    public ResponseEntity<Void> startWorkspace() {
+        return null;
+    }
+
+    @GetMapping("/workspaces/{workspaceId}")
+    public ResponseEntity<Void> enterWorkspace() {
+        return null;
+    }
+
+    @GetMapping("/workspaces/{workspaceId}/missions")
+    public ResponseEntity<Void> seeMissionsInWorkspace() {
+        return null;
+    }
+
+    @PostMapping("/workspaces/{workspaceId}/missions")
+    public ResponseEntity<Void> workMissionsInWorkspace() {
+        return null;
+    }
+
+    @GetMapping("/workspaces/{workspaceId}/tasks")
+    public ResponseEntity<Void> openTasksBoxInWorkspace() {
+        return null;
+    }
+
+}
