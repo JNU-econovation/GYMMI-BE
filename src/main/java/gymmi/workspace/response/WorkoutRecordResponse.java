4cd8a154f11b86f95ec5d37540da67b7f8edcc2f
aiaiaiai1 <cvc100326@naver.com>
2024-11-03 14:51:44 +0900

    refactor: modify response

--- src/main/java/gymmi/workspace/response/WorkoutRecordResponse.java
+++ src/main/java/gymmi/workspace/response/WorkoutRecordResponse.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 @Getter
 public class WorkoutRecordResponse {
 
+    private final Long id;
     private final String Mission;
     private final Integer count;
     private final Integer totalScore;
 
     public WorkoutRecordResponse(WorkoutRecord workoutRecord) {
+        this.id = workoutRecord.getId();
         this.Mission = workoutRecord.getMission().getName();
         this.count = workoutRecord.getCount();
         this.totalScore = workoutRecord.getSum();
