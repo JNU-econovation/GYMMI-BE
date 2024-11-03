63c2fec2ccbf85faa683bda57161ba174d754fd5
aiaiaiai1 <cvc100326@naver.com>
2024-11-03 22:24:43 +0900

    fix: delete @value

--- src/main/java/gymmi/global/S3Config.java
+++ src/main/java/gymmi/global/S3Config.java
@@ -10,9 +10,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
     @Bean
     public AmazonS3 amazonS3() {
         return AmazonS3ClientBuilder.standard()
