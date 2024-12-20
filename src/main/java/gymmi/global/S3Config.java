270ce15eb8d4cfb02c2933725fdb4c6f7a34c36f
aiaiaiai1 <cvc100326@naver.com>
2024-12-20 16:57:45 +0900

    test: add access key for s3

--- src/main/java/gymmi/global/S3Config.java
+++ src/main/java/gymmi/global/S3Config.java
@@ -1,5 +1,7 @@
 package gymmi.global;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -10,10 +12,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
+    @Value("${cloud.aws.s3.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.s3.secretKey}")
+    private String secretKey;
+
     @Bean
     public AmazonS3 amazonS3() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.AP_NORTHEAST_2)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
                 .build();
     }
 }
