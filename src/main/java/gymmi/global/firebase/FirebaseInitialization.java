7ab253b166991815f5c7c644a97242eb3b39dcdf
aiaiaiai1 <cvc100326@naver.com>
2025-01-13 12:10:59 +0900

    fix: fix duplicate initiating firebase app due to test

--- src/main/java/gymmi/global/firebase/FirebaseInitialization.java
+++ src/main/java/gymmi/global/firebase/FirebaseInitialization.java
@@ -7,11 +7,13 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
+@Profile("!test")
 public class FirebaseInitialization {
 
 
@@ -20,14 +22,23 @@ public class FirebaseInitialization {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream(path);
+        deleteIfExist();
 
+        FileInputStream serviceAccount = new FileInputStream(path);
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
         return FirebaseApp.initializeApp(options);
+    }
+
+    private void deleteIfExist() {
+        try {
+            FirebaseApp existingApp = FirebaseApp.getInstance();
+            existingApp.delete();
+        } catch (IllegalStateException ignored) {
+
+        }
     }
 
     @Bean
--- src/test/java/gymmi/GymmiApplicationTests.java
+++ src/test/java/gymmi/GymmiApplicationTests.java
@@ -1,9 +1,12 @@
 package gymmi;
 
+import gymmi.firebase.FirebaseTestConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(FirebaseTestConfig.class)
 class GymmiApplicationTests {
 
     @Test
